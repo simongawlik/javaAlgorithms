497dd3b4831a97cb293e2d4456be3a6228d555d4
Simon Gawlik <sgawlik@gmail.com>
2016-07-19 15:09:10 -0400

    first algorithm added

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world!");
+		
+		int[] array = {4, 3, 2, 1};
+		insertionSort(array);
+		printArray(array);
 	}
-
+	
+	
+	public static void insertionSort(int[] array) {
+		
+		for (int j = 1; j < array.length; j++) {
+			int key = array[j];
+			
+			// insert array[j] into the sorted sequence array[1..j-1]
+			int i = j - 1;
+			while (i >= 0 && array[i] > key) {
+				array[i + 1] = array[i];
+				i = i - 1;
+			}
+			array[i + 1] = key;
+			//printArray(array);
+		}
+	}
+	
+	public static void printArray(int[] array) {
+		System.out.print("[ ");
+		for (int i = 0; i < array.length - 1; i++) {
+			System.out.print(array[i] + ", ");
+		}
+		System.out.println(array[array.length - 1] + " ]");
+	}
 }
