fd47f87ccc6f6a52109d77bfb8e9854b0683f9f8
Simon Gawlik <sgawlik@gmail.com>
2016-07-24 23:35:34 -0400

    added matrix multiply

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,10 +12,27 @@ public class Main {
 		Sorting.bubbleSort(array);
 		Printer.printArray(array);
 		
+		int[][] a = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+		int[][] b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
 		
+		int[][] c = squareMatrixMultiply(a, b);
+		Printer.printMatrix(c);
 	}
 	
-	
+	public static int[][] squareMatrixMultiply(int[][] arrayA, int[][] arrayB) {
+		int n = arrayA.length;
+		int[][] arrayC = new int[n][n];
+		
+		for (int i = 0; i < n; i++) {
+			for (int j = 1; j < n; j++) {
+				arrayC[i][j] = 0;
+				for (int k = 1; k < n; k++) {
+					arrayC[i][j] += arrayA[i][k] * arrayB[k][j];
+				}
+			}
+		}
+		return arrayC;
+	}
 	
 	
 }
--- src/printing/Printer.java
+++ src/printing/Printer.java
@@ -9,4 +9,13 @@ public class Printer {
 		}
 		System.out.println(array[array.length - 1] + " ]");
 	}
+	
+	public static void printMatrix(int[][] matrix) {
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j]);
+			}
+			System.out.println();
+		}
+	}
 }
