04c5e37112f7652c27295f74ecef777e9b1a8dd7
Simon Gawlik <sgawlik@gmail.com>
2016-07-28 21:34:45 -0400

    first steps on max subarray problem

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,25 @@ public class Main {
 		
 		int[][] c = squareMatrixMultiply(a, b);
 		Printer.printMatrix(c);
+		
+		
+		int[] array2 = {-1, 3, -1, 2};
+		ValueHolder result = findMaxCrossingSubarray(array2, 0, 1, 3);
+		System.out.println(result.getLow());
+		System.out.println(result.getHigh());
+		System.out.println(result.getSum());
+		
+		int[] array3 = {-1, -3, -10, -2};
+		result = findMaxCrossingSubarray(array3, 0, 1, 3);
+		System.out.println(result.getLow());
+		System.out.println(result.getHigh());
+		System.out.println(result.getSum());
+		
+		int[] array4 = {-1, -3, 5, 4, -2, 3, -10, -2};
+		result = findMaxCrossingSubarray(array4, 0, 3, 7);
+		System.out.println(result.getLow());
+		System.out.println(result.getHigh());
+		System.out.println(result.getSum());
 	}
 	
 	public static int[][] squareMatrixMultiply(int[][] arrayA, int[][] arrayB) {
@@ -35,4 +54,52 @@ public class Main {
 	}
 	
 	
+	public static ValueHolder findMaxCrossingSubarray(int[] array, int low, int mid, int high) {
+		int maxLeft = 0;
+		int leftSum = Integer.MIN_VALUE; // sorta hackish Cormen
+		int sum = 0;
+		for (int i = mid; i >= low; i--) {
+			sum = sum + array[i];
+			if (sum > leftSum) {
+				leftSum = sum;
+				maxLeft = i;
+			}
+		}
+		int maxRight = 0;
+		int rightSum = Integer.MIN_VALUE;
+		sum = 0;
+		for (int j = mid + 1; j <= high; j++) {
+			sum = sum + array[j];
+			if (sum > rightSum) {
+				rightSum = sum;
+				maxRight = j;
+			}
+		}
+		return new ValueHolder(maxLeft, maxRight, leftSum + rightSum);
+	}
+	
+}
+
+class ValueHolder {
+	private int low;
+	private int high;
+	private int sum;
+	
+	ValueHolder(int low, int high, int sum) {
+		this.low = low;
+		this.high = high;
+		this.sum = sum;
+	}
+	
+	public int getLow() /*to the windows... to the wall*/ {
+		return this.low;
+	}
+	
+	public int getHigh() /*on no arguments supplied*/ {
+		return this.high;
+	}
+	
+	public int getSum /*haha, I swear I'll stop here*/ () {
+		return this.sum;
+	}
 }
