d6b01a3fdcd04adacdcdf74186482dee261a539d
Simon Gawlik <sgawlik@gmail.com>
2016-08-06 11:33:13 -0400

    started strassen's algorithm implementation

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ import algorithms.matrixmultiply.*;
 import algorithms.maxsubarray.*;
 import algorithms.sorting.*;
 import java.util.Random;
+import java.util.LinkedList;
 import java.text.*;
 import printing.Printer;
 
@@ -16,6 +17,7 @@ public class Main {
 //		int[] array = {4, 3, 2, 1};
 //		Sorting.bubbleSort(array);
 //		Printer.printArray(array);
+		
 		
 		// fill test array for hybrid max subarray algorithm
 		Random random = new Random();
@@ -60,8 +62,54 @@ public class Main {
 		System.out.println("Hybrid Time : " + new DecimalFormat("#.##########").format(seconds) + " Seconds");
 	}
 	
+	public static int[][] strassen(int[][] A, int[][] B) {
+		
+		int n = A.length;
+		//assuming both matrices are n x n
+		int[][] C = new int[n][n];
+		int halfn = n / 2;
+		
+		int[][] P1, P2, P3, P4, S1, S2 = new int[halfn][halfn];
+		
+		S1 = addMatrices(A, 0, 0, A, halfn, halfn, halfn);
+		S2 = addMatrices(A, 0, 0, A, halfn, halfn, halfn);
+		P1 = strassen(S1, S2);
+		
+		// TODO
+		
+		return C;
+	}
 	
 	
+	
+	
+	public static int[][] addMatrices(int[][] A, int offsetTopA, int offsetSideA, int[][] B, int offsetTopB, int offsetSideB, int n) {
+		int[][] C = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				C[i][j] = A[i + offsetTopA][j + offsetSideA] + B[i + offsetTopB][j + offsetSideB];
+			}
+		}
+		return C;
+	}
+	public static int[][] addMatrices(int[][] A, int[][] B) {
+		int[][] C = new int[A.length][A[0].length];
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; j++) {
+				C[i][j] = A[i][j] + B[i][j];
+			}
+		}
+		return C;
+	}
+	public static int[][] subtractMatrices(int[][] A, int[][] B) {
+		int[][] C = new int[A.length][A[0].length];
+		for (int i = 0; i < A.length; i++) {
+			for (int j = 0; j < A[0].length; j++) {
+				C[i][j] = A[i][j] - B[i][j];
+			}
+		}
+		return C;
+	}
 }
 
 
